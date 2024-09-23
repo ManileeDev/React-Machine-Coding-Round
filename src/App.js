b2c67758554ec987ef34bc5122b6c2c7a6622ffe
R S <manik9092526954@gmail.com>
2024-09-23 18:49:10 +0530

    initial push

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import { useState } from 'react';
 import './App.css';
-import Products from './components/Products';
-import useFetch from './hooks/useFetch';
-import Pagination from './components/Pagination';
+
 
 function App() {
-  const API = 'https://dummyjson.com/products?limit=100'
-  const { data } = useFetch(API)
-  let products=[];
-  const [limit,setLimit] = useState(10)
-  if (data && data.products) {
-    products = (data.products)
-  }
-  const total = Math.ceil(products.length/limit) 
-  const[currentPage,setCurrentPage] = useState(1)
-  const  end = currentPage*limit;
-  const start = end - limit;
-  const filteredProducts = products.slice(start,end) //slice(10,10)
-  console.log(currentPage)
+  
   return (
     <div className="App">
-      <Products products={filteredProducts} />
-      <Pagination setLimit={setLimit} total={total} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+      Paambu
     </div>
   );
 }
